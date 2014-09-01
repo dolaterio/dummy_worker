d2cf9844a50aeda5d747d138a9b6bd7209815f36
Albert Callarisa <shark234@gmail.com>
2014-09-01 23:10:43 +0800

    Displays STDIN and ENV

--- worker.js
+++ worker.js
@@ -1,6 +1,23 @@
 console.log("Argv: ", process.argv)
+console.log("ENV: ", process.env)
+
+process.stdin.setEncoding('utf8');
+
+from_stdin = ""
+process.stdin.on('readable', function() {
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    from_stdin = from_stdin + chunk.toString()
+  }
+});
+
+process.stdin.on('end', function() {
+});
+
 setTimeout(function() {
-  console.log("This goes to stdout")
-  console.error("This goes to stderr")
+  process.stdout.write('data from stdin:\n---\n');
+  process.stdout.write(from_stdin);
+  process.stdout.write('\n---\n');
+  process.stderr.write("This goes to stderr")
   process.exit(Math.round(Math.random() * 50))
 }, (Math.random()* 1000) + 4000)
